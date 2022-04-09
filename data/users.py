5dd5e5b6fa71d9e6e3ca5a02da63eeeec453da59
Comp Master <kalam.balshyryn@gmail.com>
2022-04-09 23:13:26 +0600

    конечный вариант

--- data/users.py
+++ data/users.py
@@ -1,7 +1,6 @@
 import datetime
 import sqlalchemy
 from data.db_session import SqlAlchemyBase
-from sqlalchemy import orm
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from sqlalchemy_serializer import SerializerMixin
